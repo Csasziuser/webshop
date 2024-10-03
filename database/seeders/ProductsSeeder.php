9b4a37ce3e8e8771740ad104d60447d49cd8671f
Császár Martin Krisztián <145013640+Csasziuser@users.noreply.github.com>
2024-10-03 18:14:01 +0200

    seeder

---
+++ database/seeders/ProductsSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('products')->insert([
+            'brand' => 'Adidas',
+            'modell' => 'Predator', 
+            'color' => 'black', 
+            'size' => 39, 
+            'stock' => 20, 
+            'price' => 15000, 
+            'product_type_id' => 2,
+        ]);
+
+        DB::table('products')->insert([
+            'brand' => 'New Balance',
+            'modell' => '500', 
+            'color' => 'black', 
+            'size' => 41, 
+            'stock' => 20, 
+            'price' => 40000, 
+            'product_type_id' => 2,
+        ]);
+
+        DB::table('products')->insert([
+            'brand' => 'Adidas',
+            'modell' => 'Ultraboost', 
+            'color' => 'black', 
+            'size' => 42, 
+            'stock' => 20, 
+            'price' => 60000, 
+            'product_type_id' => 2,
+        ]);
+    }
+}
